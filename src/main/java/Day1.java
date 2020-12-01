7ed849806b9e6c12e4666ddf17dc31d3fb8d0a4d
wei xue <jimmy.wei.xue@gmail.com>
2020-12-01 13:57:37 -0600

    COA day1 refactor

--- src/main/java/Day1.java
+++ src/main/java/Day1.java
@@ -10,6 +10,37 @@ import java.util.List;
 
 public class Day1 {
 
+    public static void main(String[] args) throws IOException, URISyntaxException {
+
+        int[] inputs = readInput();
+
+
+        // O(nlogn) complexity, space O(1)
+        Arrays.sort(inputs);
+
+        int result = produceOfTwoSumToTarget(2020, 0, inputs);
+        System.out.println("Solution for 2 sum: " + result);
+
+        result = produceOf3SumToTarget(2020, inputs);
+        System.out.println("Solution for 3 sum: " + result);
+    }
+
+    private static int[] readInput() throws URISyntaxException, IOException {
+        URL resource = Day1.class.getClassLoader().getResource("puzzle.txt");
+        Path inputFile = Path.of(resource.toURI());
+
+        try (BufferedReader inputReader = Files.newBufferedReader(inputFile)) {
+
+            List<Integer> integerList = new ArrayList<>();
+            String inputLine;
+            while ((inputLine = inputReader.readLine()) != null) {
+                integerList.add(Integer.valueOf(inputLine));
+            }
+
+            return integerList.stream().mapToInt(i -> i).toArray();
+        }
+    }
+
     /**
      * Day1 puzzle produce of 2 numbers sum up to 2020
      * <p>
@@ -17,20 +48,20 @@ public class Day1 {
      * If match found, then produce it
      * Complexity O(nlogn)
      * <p>
-     * @param target target number to sum up to
+     * @param target   target number to sum up to
      * @param startIdx start position for search
      * @param intArray sorted array of integers
      * @return produce of 2 number sum up to target
      */
     private static int produceOfTwoSumToTarget(int target, int startIdx, int[] intArray) {
         int result = -1;
-        for(int i = startIdx; i < intArray.length; i ++){
+        for (int i = startIdx; i < intArray.length; i++) {
             if (searchTargetSum(intArray, i, target)) {
                 result = i;
                 break;
             }
         }
-        if (result != -1 ) {
+        if (result != -1) {
             System.out.println("Found pair: " + intArray[result] + " and " + (target - intArray[result]));
             return intArray[result] * (target - intArray[result]);
         } else {
@@ -49,7 +80,7 @@ public class Day1 {
         int median;
 
         while (startIdx <= endIdx) {
-            median = startIdx + (endIdx - startIdx)/2;
+            median = startIdx + (endIdx - startIdx) / 2;
             if (target - matchPair == intArray[median]) {
                 return true;
             } else if (target - matchPair > intArray[median]) {
@@ -62,38 +93,14 @@ public class Day1 {
         return false;
     }
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
-        URL resource = Day1.class.getClassLoader().getResource("puzzle.txt");
-        Path inputFile = Path.of(resource.toURI());
-
-        try(BufferedReader inputReader = Files.newBufferedReader(inputFile)) {
-
-            List<Integer> integerList = new ArrayList<>();
-            String inputLine;
-            while ((inputLine = inputReader.readLine()) != null) {
-                integerList.add(Integer.valueOf(inputLine));
-            }
-
-            int[] inputs = integerList.stream().mapToInt(i->i).toArray();
-
-            // O(nlogn) complexity, space O(1)
-            Arrays.sort(inputs);
-
-            int result = produceOfTwoSumToTarget(2020, 0, inputs);
-            System.out.println("Solution found: " + result);
-
-            result = produceOf3SumToTarget(2020, inputs);
-            System.out.println("Solution found: " + result);
-        }
-    }
 
     private static int produceOf3SumToTarget(int target, int[] inputs) {
         int result = 0;
         int produceOf2 = 0;
-        for(int i = 0; i < inputs.length; i++){
+        for (int i = 0; i < inputs.length; i++) {
             int match1 = inputs[i];
-            int produce2 = produceOfTwoSumToTarget(target - match1, i+1, inputs);
-            if(produce2 != -1) {
+            int produce2 = produceOfTwoSumToTarget(target - match1, i + 1, inputs);
+            if (produce2 != -1) {
                 System.out.println("Find match: " + match1);
                 return match1 * produce2;
             }
